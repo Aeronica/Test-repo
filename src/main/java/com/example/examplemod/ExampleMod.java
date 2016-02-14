073233618e557861389d862f2abf94342fd6a217
Aeronica <Aeronica@users.noreply.github.com>
2016-02-13 21:24:36 -0600

    Version updates

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -9,7 +9,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.7.10-2016-02-14";
     public ExampleMod instance;
 	
     @EventHandler
