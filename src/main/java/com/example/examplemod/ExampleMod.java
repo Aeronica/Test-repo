06e8dd03cfe93c60fa063ad5416ceafbdcb67ecf
Aeronica <pmboese@gmail.com>
2016-12-09 11:08:38 -0600

    Bring master to 1.10.2.

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.9-2016-10-20";
+    public static final String VERSION = "1.10-2016-12-09";
     public static final CreativeTabs TAB_EXAMPLE = new ExampleTab(CreativeTabs.getNextID(), ExampleMod.MODID);
     
     @EventHandler
